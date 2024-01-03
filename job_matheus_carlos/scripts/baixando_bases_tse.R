d1cb3798040ec1bf1ddfb0f1817882fdc4e50283
Alan Cavalcanti <alan.cavalcanti@ufpe.br>
2024-01-03 15:03:00 -0300

    script atualizado

--- job_matheus_carlos/scripts/baixando_bases_tse.R
+++ job_matheus_carlos/scripts/baixando_bases_tse.R
@@ -1,6 +1,11 @@
-if (!require("devtools")) install.packages("devtools")
-devtools::install_github("silvadenisson/electionsBR")
-1
+install.packages("vctrs")
+install.packages("electionsBR")
+
+library(electionsBR)
+
+df_2014 <- candidate_fed(year = 2014)
+df_2018 <- candidate_fed(year = 2018)
+df_2022 <- candidate_fed(year = 2022)
 
 
 years <- c(2014, 2018, 2022)
